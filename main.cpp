08a09beb776f7b4153c0d9ae6c07dde645d40ba0
Nathan Reusch <73974845+ReusNat@users.noreply.github.com>
2023-10-14 14:18:46 -0500

    Shows usage of functionality

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
-
-
+#include <random>
+#include <string>
+#include "Building.h"
 
 int main() {
+	srand(9898989898);
 	std::cout << "Hello" << std::endl;
-	
+	std::vector<Person> people;
+
+	for (int i = 0; i < 50; i++) {
+		people.push_back(Person(std::to_string(i), rand() % 10, rand() % 10));
+	}
+
+	Building build = Building(10, people);
+
+	build.printBuilding();
+
+	return 0;
 }
