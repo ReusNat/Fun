0c3dd8715079f7d9582b7dabc35c436c215e0016
Nathan Reusch <73974845+ReusNat@users.noreply.github.com>
2023-10-23 14:04:06 -0500

    Update person.h

--- person.h
+++ person.h
@@ -1,1 +1,36 @@
 #pragma once
+
+class Person {
+
+private:
+	std::string name;
+	int desiredFloor;
+	int startFloor;
+
+public:
+
+	// Constructor with passed in values
+	Person(std::string personName, int startFloor, int desiredFloor) {
+		this->name = personName;
+		this->startFloor = startFloor;
+		this->desiredFloor = desiredFloor;
+	}
+
+	int dest() {
+		return this->desiredFloor;
+	}
+
+	int startFloor() {
+		return this->startFloor;
+	}
+
+	std::string getName() {
+		return this->name;
+	}
+
+	// Destructor
+	~Person() {
+		delete this;
+	}
+
+};
