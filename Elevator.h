23782b8c3a453e0dc8f928b827809bb6a267f3b9
Nathan Reusch <73974845+ReusNat@users.noreply.github.com>
2023-10-24 15:17:54 -0500

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,21 +1,74 @@
 #pragma once
 #include <vector>
-#include "person.h"
+#include <algorithm>
+#include "Person.h"
+#include "Common.h"
 
 class Elevator {
 
 	private:
 		int currFloor;
+		int topFloor;
 		bool goingUp = 1;
 		std::vector<Person> people;
 
 	public:
 		Elevator() {
 			this->currFloor = 0;
+			this->topFloor = 0;
+		}
+		Elevator(int topFloor) {
+			this->currFloor = 0;
+			this->topFloor = topFloor;
 		}
 
-		void move(int floorToGo) {
+		void move(Floor& floor) {
 
+			while (goingUp) {
+				if (currFloor == topFloor) {
+					goingUp = 0;
+				}
+
+
+				for (Person person : people) {
+					if (currFloor == person.dest()) {
+						auto p = std::find_if(people.begin(), people.end(),
+							[&person](Person p) {
+								return person == p;
+							});
+						people.erase(p);
+
+					}
+				}
+
+				for (Person person : floor.goingUp) {
+					people.push_back(person);
+				}
+				floor.goingUp.clear();
+
+				currFloor++;
+			}	
+
+			while (currFloor != 0) {
+				
+				for (Person person : people) {
+					if (currFloor == person.dest()) {
+						auto p = std::find_if(people.begin(), people.end(),
+							[&person](Person p) {
+								return person == p;
+							});
+						people.erase(p);
+
+					}
+				}
+
+				for (Person person : floor.goingDown) {
+					people.push_back(person);
+				}
+				floor.goingDown.clear();
+
+				currFloor--;
+			}
 		}
 
 		int getCurrFloor() { return this->currFloor; }
